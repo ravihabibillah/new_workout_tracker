e2b9a7b4d4a9252c9224a15cff4a6d7d179288b7
Muhammad Ravi Habibillah <ravihabibillah44@gmail.com>
2026-05-19 11:15:47 +0700

    style: use green background for success snackbar

--- lib/core/extensions/context_extensions.dart
+++ lib/core/extensions/context_extensions.dart
@@ -37,7 +37,7 @@ extension ContextExtensions on BuildContext {
     ScaffoldMessenger.of(this).showSnackBar(
       SnackBar(
         content: Text(message),
-        backgroundColor: isError ? Colors.red : null,
+        backgroundColor: isError ? Colors.red : Colors.green,
         behavior: SnackBarBehavior.floating,
       ),
     );
